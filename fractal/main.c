b4861563bc074f171eeaa232b2e9b71a39c79143
jdoutreloux <justine.doutreloux@hotmail.be>
2016-04-18 20:08:52 +0200

    .

--- fractal/main.c
+++ fractal/main.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "fractal.h"
+#include "libfractal/fractal.h"
+#include <semaphore.h>
+
+pthread_mutex_t mutex;
+sem_t empty;
+sem_t full;
+
+/*
+ * Cette fonction gérera la partie lecture effectuée par les threads
+ */
+void *reader(void *file)
+{
+    return NULL;
+}
+
+
+/*
+ * Cette fonction gérera la partie écriture effectuée par les threads
+ */
+void *writer(void)
+{
+    return NULL;
+}
+
+
 
 int main()
 {
-    /* TODO */
 
-    return 0;
 }
